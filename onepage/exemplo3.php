048eeb8f31423b54f0e1382e4052f835c963818c
Daniela Yassuda <danielayassuda@gmail.com>
2015-05-27 21:12:12 -0300

    Alterações no esquema de BD; funções de manipulação de BD dos Eventos

--- onepage/exemplo3.php
+++ onepage/exemplo3.php
@@ -1,5 +1,6 @@
 <?php
 require_once "php/conexao.php";
+require_once "php/evento_classes.php";
 
 global $db;
 
@@ -11,3 +12,23 @@ $todosUsuarios = $usuarios->fetchAll();
 foreach($todosUsuarios as $umUsuario){
 	var_dump($umUsuario);
 }
+
+$aula = new Aula();
+
+$aula->setData("11/06/2015");
+$aula->setHorario("11:00");
+$aula->setInstrumento("Guitarra");
+$aula->setNivel("Iniciante");
+$aula->setSala("Sala 2");
+$aula->setTipo("Reposição");
+$aula->setPresenca(0);
+$aula->setAlunoId(0);
+$aula->setProfessorId(0);
+
+$aula->insert();
+
+$teste = new Aula();
+
+$teste->select($aula->getId());
+
+var_dump($teste);

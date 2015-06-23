609c05040dfecfd55be69cf2b8422c549d4b8ce3
ThiagoLira <thlira15@gmail.com>
2015-06-23 16:09:28 -0300

    corrigido bug no login

--- onepage/login.php
+++ onepage/login.php
@@ -5,16 +5,25 @@ session_start();
 
 $interface = new Aplicacao();
 
+ 
+
 if(isset($_POST['user']) && isset($_POST['pass'])){
     $username = $_POST['user'];
-    $hash = $_POST['pass'];
+    $senha = $_POST['pass'];
+    $hash =  md5 ( $senha  );
+    
+    
+
 
     if($interface->login($username,$hash)){
         header('Location: meuPainel.php');
         exit();
     }
     else{
+
         $_SESSION['erro'] = "Nome de usuário e/ou senha incorretos!";
+        
+
         header('Location: loginAreaUsuario.php');
         exit();
     }
